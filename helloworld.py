720b46dd289bb562f8028d627913fef441a97809
ajcote <alexanderjohncote@gmail.com>
2020-03-22 18:33:11 -0400

    file helloworld.py added

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+userName = input("hello! Please enter your Facebook username!")
+
+userPassword = input("Hello! Please enter your Facebook password!")
+
+if(userName.lower() == "alexander" and userPassword == "programmingRocks!"):
+    print("Welcome to Facebook")
+else:
+    print("Wrong credentials, please try again!")
